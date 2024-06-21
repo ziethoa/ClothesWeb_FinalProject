c9fa35acd070e4fb9d73778b056b5c835cccc021
ziethoa <viethoa12307@gmail.com>
2024-06-21 23:31:24 +0700

    quản lí sản phầm - bắt đầu

---
+++ finalproject/ClothesWeb/ClothesWeb/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,24 @@
+﻿using ClothesWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothesWeb.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/ProductCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
