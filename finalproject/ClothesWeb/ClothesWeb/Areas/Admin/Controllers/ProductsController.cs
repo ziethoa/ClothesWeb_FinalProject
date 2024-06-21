a982a2b8259ae3014585fde5d402ccf5c8191fd0
ziethoa <viethoa12307@gmail.com>
2024-06-21 14:51:17 +0700

    cập nhật một chút

---
+++ finalproject/ClothesWeb/ClothesWeb/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using ClothesWeb.Models;
+using ClothesWeb.Models.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothesWeb.Areas.Admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/Products
+        public ActionResult Index(int ? page)
+        {
+            IEnumerable<Product> items = db.Products.OrderByDescending(x => x.Id);
+            var pageSize = 10;//bản ghi
+            if (page == null)
+            {
+                page = 1;
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
+            return View(items);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
