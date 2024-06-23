2e3a23aa2c7253c3204c13d6e0c459887df1d0fe
ziethoa <viethoa12307@gmail.com>
2024-06-23 18:51:01 +0700

    gắn template

---
+++ finalproject/ClothesWeb/ClothesWeb/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using ClothesWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothesWeb.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Partial_itemsByCateId(int cateid)
+        {
+            var items = db.Products.Where(x => x.ProductCategoryId == cateid).ToList();
+            return PartialView(items);
+        }
+    }
+}
